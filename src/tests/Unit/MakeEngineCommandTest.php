0083b735e0d66936c576cf4ae1ed3f088dcebe46
vahid aghazade <v.opsource@gmail.com>
2025-01-30 20:59:47 +0330

    unittest files for testing

---
+++ src/tests/Unit/MakeEngineCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Opsource\QueryAdapter\Commands;
+
+use PHPUnit\Framework\TestCase;
+
+class MakeEngineCommandTest extends TestCase
+{
+
+    public function testHandle()
+    {
+        $command = new MakeEngineCommand();
+        $this->assertTrue(true);
+
+    }
+}
